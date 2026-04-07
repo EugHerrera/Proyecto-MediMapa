1043301c6a45620993bd5f652a1697754fc17a8f
EugeNicoHe <eug.herrera@duocuc.cl>
2026-04-06 21:19:01 -0400

    Estructura inicial del proyecto
    Creacion de carpetas documentacion, gestion, producto

---
+++ Producto/medimapa/medimapa/src/main/java/cl/duoc/medimapa/MedimapaApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.medimapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedimapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedimapaApplication.class, args);
+	}
+
+}
---
+++ Producto/medimapa/medimapa/src/test/java/cl/duoc/medimapa/MedimapaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.medimapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedimapaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
