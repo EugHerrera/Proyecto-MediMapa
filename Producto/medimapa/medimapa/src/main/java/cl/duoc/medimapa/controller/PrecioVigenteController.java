9ccc69b1a0fab8cdb13bec95f4e75f9581c63606
EugeNicoHe <eug.herrera@duocuc.cl>
2026-04-12 01:56:51 -0400

    CORS
    CONFIGURACION CORS

--- Producto/medimapa/medimapa/src/main/java/cl/duoc/medimapa/controller/PrecioVigenteController.java
+++ Producto/medimapa/medimapa/src/main/java/cl/duoc/medimapa/controller/PrecioVigenteController.java
@@ -3,6 +3,9 @@ package cl.duoc.medimapa.controller;
 import cl.duoc.medimapa.model.PrecioVigente;
 import cl.duoc.medimapa.repository.PrecioVigenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+// IMPORTANTE: Agregamos estas dos importaciones nuevas
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +14,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/scraper")
+// ⬇️ ESTA ES LA LÍNEA CLAVE QUE DEBES AGREGAR ⬇️
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class PrecioVigenteController {
 
     @Autowired
     private PrecioVigenteRepository precioRepo;
 
-    // Sebastián y Misael llamarán a esta ruta para ver los precios que raspó tu bot
     @GetMapping("/precios")
     public List<PrecioVigente> obtenerTodosLosPrecios() {
+        System.out.println("📡 Enviando precios al Frontend...");
         return precioRepo.findAll();
     }
 }
